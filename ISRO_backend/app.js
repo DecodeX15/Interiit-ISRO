25151aadd6abf774a54f812122b04e0e2e400803
Vaibhaviitian <aryanvaibhav74@gmail.com>
2025-12-05 14:47:37 +0530

    fixing axios error

--- ISRO_backend/app.js
+++ ISRO_backend/app.js
@@ -4,14 +4,19 @@ import express from "express";
 import cors from "cors";
 const app = express();
 import cookieParser from "cookie-parser";
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 import { interactive_router } from "./rotues/interactive_mode.router.js";
 import { evaluation_router } from "./rotues/evaluation_mode.router.js";
-app.use("/api", interactive_router); 
+app.use("/api", interactive_router);
 app.use("/api", evaluation_router);
 console.log("aaya hu app.js ");
 app.get("/", (req, res) => {
